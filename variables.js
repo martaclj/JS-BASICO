6ce7dafc299025e6e2b806226a2480a605cf8cbe
martaclj <104948192+martaclj@users.noreply.github.com>
2022-07-23 17:34:37 +0200

    valores

---
+++ variables.js
@@ -0,0 +1,20 @@
+var nombre = "Oscar";
+
+// declarar 
+var edad;
+
+edad 
+// inicializar
+edad = 30;
+
+edad 
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
+
