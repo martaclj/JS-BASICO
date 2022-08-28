52cfd0f98909896a1a807e66ce4ed421c242ff14
martaclj <104948192+martaclj@users.noreply.github.com>
2022-08-28 22:32:51 +0200

    loops

---
+++ loops.js
@@ -0,0 +1,9 @@
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
+
+function saludarEstudiantes(estudiante){
+  console.log(`Hola, ${estudiante}`);
+}
+
+for(var i = 0; i < estudiantes.length; i++) {
+  saludarEstudiantes(estudiantes[i])
+}
